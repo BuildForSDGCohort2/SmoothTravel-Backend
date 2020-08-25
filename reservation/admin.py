da891a813dfd13ca8df2029ebe5059056c7c7fb7
eneotujoe <eneotujoe@gmail.com>
2020-08-25 18:01:08 +0100

    removed unused import statements

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .model import Flight, Hotel
 
-# Register your models here.
+admin.site.register(Flight)
+admin.site.register(Hotel)
--- smoothtravel/settings.py
+++ smoothtravel/settings.py
@@ -3,7 +3,6 @@ import os
 from dotenv import load_dotenv
 import django_heroku 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -12,9 +11,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('SMOOTHTRAVEL_SECRET_KEY')
 DEBUG = os.getenv('SMOOTHTRAVEL_DEBUG_VALUE')
 
-
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -58,7 +55,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'smoothtravel.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -68,7 +64,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -88,7 +83,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -101,7 +95,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
