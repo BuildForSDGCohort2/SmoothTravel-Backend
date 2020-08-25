d61b3ad0d937e2b4681b578418fe53603364e1df
eneotujoe <eneotujoe@gmail.com>
2020-08-25 18:22:10 +0100

    edited for codacy review

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from .models import Flight, Hotel
+# from django.contrib import admin
+# from .models import Flight, Hotel
 
-admin.site.register(Flight)
-admin.site.register(Hotel)
+# admin.site.register(Flight)
+# admin.site.register(Hotel)
