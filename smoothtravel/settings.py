8195f6cf3e80a6ea8ea5633aca92f6395ec794fa
eneotujoe <eneotujoe@gmail.com>
2020-08-25 18:11:10 +0100

    edited for codacy review

--- smoothtravel/settings.py
+++ smoothtravel/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 import django_heroku 
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
